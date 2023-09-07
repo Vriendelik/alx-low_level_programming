b559bfcf5d5923bbee85395f975cce6374aa8a92
Nteboheleng <ntebohelengd@gmail.com>
2023-09-07 15:35:06 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - main function
+*
+*Return: 0 when successful
+*/
+int main(void)
+{
+printf("Programming is like building a multilingual puzzle\n");
+return (0);
+}
